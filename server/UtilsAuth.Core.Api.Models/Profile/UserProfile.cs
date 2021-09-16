043912f30819fdfa5f205d7c354df9a4afce1210
Drazen Mrvos <dmrvos@gmail.com>
2021-09-16 11:53:55 +0200

    Add roles to default user profile

--- server/UtilsAuth.Core.Api.Models/Profile/UserProfile.cs
+++ server/UtilsAuth.Core.Api.Models/Profile/UserProfile.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace UtilsAuth.Core.Api.Models.Profile
 {
     public class UserProfile : IUserProfile
     {
-        public IEnumerable<AuthUtilsClaim> Claims { get; set; }
+        public IEnumerable<AuthUtilsClaim> Claims { get; set; } = Enumerable.Empty<AuthUtilsClaim>();
+        public IEnumerable<string> Roles => Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value);
     }
 }
