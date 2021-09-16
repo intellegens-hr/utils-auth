f5ff9de23ca96c1a3e3c9ef4635c20432b6b2e0f
Drazen Mrvos <dmrvos@gmail.com>
2021-09-16 12:31:46 +0200

    Nuget preparation

---
+++ server/nuget/nuget_publish.ps1
@@ -0,0 +1,18 @@
+cd ..
+
+get-childitem -include *.nupkg -recurse | remove-item
+
+dotnet clean
+dotnet build -c Release
+dotnet pack -c Release
+
+copy .\UtilsAuth.Core\bin\Release\*.nupkg .
+copy .\UtilsAuth.DbContext\bin\Release\*.nupkg .
+copy .\UtilsAuth.Services\bin\Release\*.nupkg .
+copy .\UtilsAuth.Core.Api.Models\bin\Release\*.nupkg .
+copy .\UtilsAuth.Core.Api.Controllers\bin\Release\*.nupkg .
+copy .\UtilsAuth.Extensions\bin\Release\*.nupkg .
+
+dotnet nuget push "*.nupkg" --api-key $env:IntellegensNugetApiKey --source https://api.nuget.org/v3/index.json
+
+del *.nupkg
