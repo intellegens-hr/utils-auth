1be985b68c769cb288fe9be8ab540124829c0a50
Drazen Mrvos <dmrvos@gmail.com>
2021-10-18 08:27:50 +0200

    Add extensions to fetch user id and username from ClaimsPrincipal

---
+++ server/UtilsAuth.Extensions/IdentityExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using UtilsAuth.Services;
+
+namespace UtilsAuth.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            var claimId = claimsPrincipal.Claims.First(x => x.Type == ClaimsConstants.ClaimId).Value;
+            return Convert.ToInt32(claimId);
+        }
+
+        public static string GetUsername(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.Claims.First(x => x.Type == ClaimsConstants.ClaimUsername).Value;
+        }
+    }
+}
